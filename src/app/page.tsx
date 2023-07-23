43de0919a4b00724e4189906c989203b259a03cb
JaykumarPatel4802 <patel.jay4802@gmail.com>
2023-07-22 20:29:48 -0500

    make table prettier

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -86,7 +86,7 @@ export default function Home() {
               <input onChange={handleStateChange} placeholder="State of Application" value={state} />
               <input onChange={handleDeadlineChange} placeholder="Deadline" value={deadline} />
               <input onChange={handleUrlChange} placeholder="Application Url" value={url} />
-              <button onClick={handleSubmit}>Search</button>
+              <button onClick={handleSubmit}>Add Application</button>
           </form>
       </div>
     );
@@ -94,9 +94,9 @@ export default function Home() {
 
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-          <Applications />
+      <div className="flex flex-col z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
         {createApplicationForm()}
+        <Applications />
       </div>
     </main>
   )
--- src/components/Applications.tsx
+++ src/components/Applications.tsx
@@ -17,32 +17,50 @@ const Applications = (props : any) => {
     
         var applicationsList = applications.map((item: any) => {
             // console.log("getApplicationsList")
-            return <Application name={item.name} description={item.description} key={item.id} />
+            return <Application item={item} key={item.id} />
         })
         // console.log(applicationsList);
         return applicationsList;
     }
 
     return (
-        <table className='table-auto'>
-            <thead>
-                <tr>
-                    <th className='px-4 py-2'>Name</th>
-                    <th className='px-4 py-2'>Description</th>
-                </tr>
-            </thead>
-            <tbody>
-                {ApplicationsList()}
-            </tbody>
-        </table>
+        <section className={"container px-4 mx-auto" + (' dark') }>
+            <div className="flex flex-col">
+                <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+                    <div className="inline-block min-w-full py-2 align-middle md:px-6 lg:px-8">
+                        <div className='overflow-hidden border border-gray-200 dark:border-gray-700 md:rounded-lg'>
+                            <table className='min-w-full divide-y divide-gray-200 dark:divide-gray-700'>
+                                <thead className='bg-gray-50 dark:bg-gray-800'>
+                                    <tr>
+                                        <th scope='col' className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400'>Name</th>
+                                        <th scope='col' className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400'>Description</th>
+                                        <th scope='col' className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400'>Role</th>
+                                        <th scope='col' className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400'>State</th>
+                                        <th scope='col' className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400'>Deadline</th>
+                                        <th scope='col' className='px-4 py-3.5 text-sm font-normal text-left rtl:text-right text-gray-500 dark:text-gray-400'>URL</th>
+                                    </tr>
+                                </thead>
+                                <tbody className='bg-white divide-y divide-gray-200 dark:divide-gray-700 dark:bg-gray-900'>
+                                    {ApplicationsList()}
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
     );
 }
 
 const Application = (props: any) => {
     return (
         <tr>
-            <td>{props.name}</td>
-            <td>{props.description}</td>
+            <td className='px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap'>{props.item.name}</td>
+            <td className='px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap'>{props.item.description}</td>
+            <td className='px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap'>{props.item.role}</td>
+            <td className='px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap'>{props.item.state}</td>
+            <td className='px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap'>{props.item.deadline}</td>
+            <td className='px-4 py-4 text-sm text-gray-500 dark:text-gray-300 whitespace-nowrap'>{props.item.url}</td>
         </tr>
     );
 }
